328972d48dcd9f77db591f7e04ffe9b405ebf783
Rajeev Menon <rajeevrmenon97@gmail.com>
2023-04-06 19:27:32 -0500

    Create a bridge

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/rajeevrmenon97/vxlan-cni
 
 go 1.20
 
-require github.com/containernetworking/cni v1.1.2
+require (
+	github.com/containernetworking/cni v1.1.2
+	github.com/vishvananda/netlink v1.1.0
+)
+
+require (
+	github.com/vishvananda/netns v0.0.0-20191106174202-0a2b9b5464df // indirect
+	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
+)
--- plugin/main.go
+++ plugin/main.go
@@ -4,9 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"strings"
+	"syscall"
 
 	"github.com/containernetworking/cni/pkg/skel"
 	"github.com/containernetworking/cni/pkg/version"
+	"github.com/vishvananda/netlink"
 )
 
 var debug bool = true
@@ -29,6 +31,39 @@ func cmdAdd(args *skel.CmdArgs) error {
 	network := Network{}
 	if err := json.Unmarshal(args.StdinData, &network); err != nil {
 		return err
+	}
+
+	brName := "br-" + network.Name
+	var br *netlink.Bridge
+	var ok bool
+
+	l, err := netlink.LinkByName(brName)
+	if err == nil {
+		br, ok = l.(*netlink.Bridge)
+		if !ok {
+			return fmt.Errorf("%q already exists but is not a bridge", brName)
+		}
+	} else {
+		if !strings.Contains(err.Error(), "Link not found") {
+			return fmt.Errorf("Error while querying for link: %v", err)
+		}
+
+		br = &netlink.Bridge{
+			LinkAttrs: netlink.LinkAttrs{
+				Name:   brName,
+				MTU:    1500,
+				TxQLen: -1,
+			},
+		}
+
+		err := netlink.LinkAdd(br)
+		if err != nil && err != syscall.EEXIST {
+			return err
+		}
+
+		if err := netlink.LinkSetUp(br); err != nil {
+			return err
+		}
 	}
 
 	return nil
